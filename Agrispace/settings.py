ef7007e487c947e8e3672b2ded161287a52887c6
Adams Temidire A <gr8temi@gmail.com>
2019-11-22 17:48:24 +0100

    settings

--- Agrispace/settings.py
+++ Agrispace/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config
+from dotenv import load_dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+from pathlib import Path  # python3 only
+env_path = Path(".") / '.env'
+load_dotenv(dotenv_path=env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -92,7 +96,7 @@ else:
         }
     }
 
-print(config("HOST", cast=str, default="I can't find the env"))
+print("Here is my env HOST: "+os.getenv("HOST"))
 # print(config())
 
 # Password validation
