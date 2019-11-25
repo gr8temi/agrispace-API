84e0183127865b8ba57d23e90b64cab9d81ac226
Adams Temidire A <gr8temi@gmail.com>
2019-11-25 15:53:21 +0100

    information api

--- home/tests.py
+++ home/tests.py
@@ -1,5 +1,11 @@
 from rest_framework.test import APITestCase
+from rest_framework import status
 # Create your tests here.
 class Test(APITestCase):
     def test_travis(self):
         self.assertEqual(1+1,2)
+
+class GetInformation(APITestCase):
+    def test_get_information(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index')
+    path('', views.Info.as_view() , name='index')
 ]
